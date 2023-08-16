91742e5b9783f03e7664d1b26be9d430d9f17f6c
Nicode611 <nicode611@gmail.com>
2023-08-16 20:12:49 +0200

    20:12

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer/footer.css">
+    <link rel="stylesheet" href="header/header.css">
     <title>Garage V. Parrot</title>
 </head>
 <body>
     <?php
-    require "/Header/header.html";
+    require "Header/header.html";
+    require "footer/footer.html";
     ?>
 </body>
 </html>
