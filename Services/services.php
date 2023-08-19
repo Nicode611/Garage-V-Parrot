7fee48c2876750a4da07455e64747b3323d53062
Nicode611 <nicode611@gmail.com>
2023-08-19 17:37:27 +0200

    17:37

--- Services/services.php
+++ Services/services.php
@@ -14,6 +14,34 @@
         require "../Header/header.html";
     ?>
 
+    <main>
+    <div class="glow"></div>
+        <h2 class="main-title">Nos services</h2>
+        <div class="services-section">
+            <div class="service">
+                <img class="service-img" src="../img/réparation auto.jpg" alt="">
+                <div class="service-infos">
+                    <h3 class="service-title">Réparation auto</h3>
+                    <p class="service-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod scelerisque faucibus. Curabitur vitae arcu id odio placerat rhoncus </p>
+                </div>
+            </div>
+            <div class="service">
+                <img class="service-img" src="../img/nettoyage voitures.jpg" alt="">
+                <div class="service-infos">
+                    <h3 class="service-title">Lavage intégral auto</h3>
+                    <p class="service-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod scelerisque faucibus. Curabitur vitae arcu id odio placerat rhoncus </p>
+                </div>
+            </div>
+            <div class="service">
+                <img class="service-img" src="../img/vente de véhicule d'occasion.jpg" alt="">
+                <div class="service-infos">
+                    <h3 class="service-title">Véhicules d’occasion</h3>
+                    <p class="service-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod scelerisque faucibus. Curabitur vitae arcu id odio placerat rhoncus </p>
+                </div>
+            </div>
+        </div>
+    </main>
+
     <?php
         require "../Footer/footer.html";
     ?>
