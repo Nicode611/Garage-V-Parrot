75a7d4e2eafec857efbc89056ba6d61155ecb9f1
Nicode611 <nicode611@gmail.com>
2023-08-19 14:30:56 +0200

    14:30

--- Contact/contact.php
+++ Contact/contact.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="services.css">
+    <link rel="stylesheet" href="contact.css">
     <link rel="stylesheet" href="../Header/header.css">
     <link rel="stylesheet" href="../Footer/footer.css">
     <title>Garage V. Parrot</title>
