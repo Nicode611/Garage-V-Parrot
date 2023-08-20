43e1209113c37d5999fdbb641ab985bce739fb3e
Nicode611 <nicode611@gmail.com>
2023-08-20 11:47:49 +0200

    11:47

--- Contact/contact.php
+++ Contact/contact.php
@@ -14,7 +14,28 @@
     require "../Header/header.html";
 ?>
 
+<main>
+    <h2>Contactez nous</h2>
 
+    <div class="form-container">
+        <div class="glow"></div>
+        <form class="form" action="traitement.php" method="POST">
+            <div class="first-line">
+                <input type="text" placeholder="Nom" id="nom" name="nom" required>
+        
+                <input type="tel" placeholder="Téléphone" id="telephone" name="telephone">
+            </div>
+
+            <br><br>
+    
+            <input type="email" placeholder="Email" id="email" name="email" required><br><br>
+    
+            <textarea id="message" placeholder="Message" name="message" rows="4" cols="50" required></textarea><br><br>
+    
+            <input id="send" type="submit" value="Envoyer">
+        </form>
+    </div>
+</main>
 
 <?php
     require "../Footer/footer.html";
