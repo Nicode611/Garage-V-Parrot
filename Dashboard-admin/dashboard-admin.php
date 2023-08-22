c7b448f614cc919dbe80d096857460d888d236b5
Nicode611 <nicode611@gmail.com>
2023-08-22 16:17:25 +0200

    16:17

---
+++ Dashboard-admin/dashboard-admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard-admin.css">
+    <title>Garage V. Parrot</title>
+</head>
+<body>
+    
+<?php 
+    require "../Header/header.html";
+?>
+</body>
+</html>
