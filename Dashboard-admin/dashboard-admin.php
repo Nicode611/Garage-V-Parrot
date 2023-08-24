522654806c35e2ec24fe43e84837e1777be4d7f2
Nicode611 <nicode611@gmail.com>
2023-08-24 15:38:30 +0200

    15:38

--- Dashboard-admin/dashboard-admin.php
+++ Dashboard-admin/dashboard-admin.php
@@ -17,7 +17,7 @@
         <div class="sidebar">
             <nav class="sidebar-nav">
                 <ul class="sidebar-ul">
-                    <li class="sidebar-li sidebar-services sidebar-active">Services</li>
+                    <li class="sidebar-li sidebar-services">Services</li>
                     <li class="sidebar-li sidebar-occasions">Occasions</li>
                     <li class="sidebar-li sidebar-horaires">Horaires</li>
                     <li class="sidebar-li sidebar-employes">Employés</li>
@@ -27,7 +27,7 @@
 
 
         <!-- Partie services -->
-        <div class="dashboard-services">
+        <div class="dashboard-services hide">
             <div class="glow"></div>
             <div class="services-section">
                 <div class="service">
@@ -39,7 +39,6 @@
                     </div>
                 </div>
                 <div class="service">
-                <div class="service-delete"></div>
                 <svg viewBox="-102.4 -102.4 1228.80 1228.80" fill="#ffffff" class="service-delete" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="6.144"></g><g id="SVGRepo_iconCarrier"><path d="M512 897.6c-108 0-209.6-42.4-285.6-118.4-76-76-118.4-177.6-118.4-285.6 0-108 42.4-209.6 118.4-285.6 76-76 177.6-118.4 285.6-118.4 108 0 209.6 42.4 285.6 118.4 157.6 157.6 157.6 413.6 0 571.2-76 76-177.6 118.4-285.6 118.4z m0-760c-95.2 0-184.8 36.8-252 104-67.2 67.2-104 156.8-104 252s36.8 184.8 104 252c67.2 67.2 156.8 104 252 104 95.2 0 184.8-36.8 252-104 139.2-139.2 139.2-364.8 0-504-67.2-67.2-156.8-104-252-104z" fill=""></path><path d="M707.872 329.392L348.096 689.16l-31.68-31.68 359.776-359.768z" fill=""></path><path d="M328 340.8l32-31.2 348 348-32 32z" fill=""></path></g></svg>
                     <img class="service-img" src="../img/nettoyage voitures.jpg" alt="">
                     <div class="service-infos">
@@ -48,7 +47,6 @@
                     </div>
                 </div>
                 <div class="service">
-                <div class="service-delete"></div>
                 <svg viewBox="-102.4 -102.4 1228.80 1228.80" fill="#ffffff" class="service-delete" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="6.144"></g><g id="SVGRepo_iconCarrier"><path d="M512 897.6c-108 0-209.6-42.4-285.6-118.4-76-76-118.4-177.6-118.4-285.6 0-108 42.4-209.6 118.4-285.6 76-76 177.6-118.4 285.6-118.4 108 0 209.6 42.4 285.6 118.4 157.6 157.6 157.6 413.6 0 571.2-76 76-177.6 118.4-285.6 118.4z m0-760c-95.2 0-184.8 36.8-252 104-67.2 67.2-104 156.8-104 252s36.8 184.8 104 252c67.2 67.2 156.8 104 252 104 95.2 0 184.8-36.8 252-104 139.2-139.2 139.2-364.8 0-504-67.2-67.2-156.8-104-252-104z" fill=""></path><path d="M707.872 329.392L348.096 689.16l-31.68-31.68 359.776-359.768z" fill=""></path><path d="M328 340.8l32-31.2 348 348-32 32z" fill=""></path></g></svg>
                     <img class="service-img" src="../img/vente de véhicule d'occasion.jpg" alt="">
                     <div class="service-infos">
