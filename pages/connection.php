a83594e790e881c7fcba3001ffc361277c1e7629
Nicode611 <nicode611@gmail.com>
2023-08-26 20:31:46 +0200

    20:31

--- pages/connection.php
+++ pages/connection.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="connection.css">
-    <link rel="stylesheet" href="../Header/header.css">
+    <link rel="stylesheet" href="../assets/css/connection.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
     <title>Garage V. Parrot</title>
 </head>
 <body>
     
     <?php
-        require "../Header/header.html";
+        require "../includes/header.html";
     ?>
 
     <main>
