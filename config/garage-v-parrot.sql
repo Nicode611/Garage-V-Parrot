fcd94dcc653df2c5328f6fde149a5948989269a8
Nicode611 <nicode611@gmail.com>
2023-08-30 17:37:56 +0200

    17:37

--- config/garage-v-parrot.sql
+++ config/garage-v-parrot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 30 août 2023 à 17:33
+-- Généré le : mer. 30 août 2023 à 17:36
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,6 +20,68 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `garage_v_parrot`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `id` int(11) NOT NULL,
+  `avis` text NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`id`, `avis`, `nom`) VALUES
+(1, 'Trop sympa a l\'accueil ', 'Mathieu nougaré');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `image` text NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `services`
+--
+
+INSERT INTO `services` (`id`, `image`, `nom`, `description`) VALUES
+(1, 'https://servzebi/zebi.com', 'Réparation de pare brise', 'On répare les pare brises ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `prénom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  `telephone` int(10) NOT NULL,
+  `code_employé` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `prénom`, `nom`, `email`, `mdp`, `telephone`, `code_employé`) VALUES
+(1, 'Nicolas', 'Guigay', 'nicolas.guigay@gmail.com', 'Nicolas6789?', 658741255, 78451);
 
 -- --------------------------------------------------------
 
@@ -49,6 +111,24 @@ INSERT INTO `vehicules` (`id`, `modele`, `année`, `kilométrage`, `prix`, `desc
 --
 
 --
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
@@ -57,6 +137,24 @@ ALTER TABLE `vehicules`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `vehicules`
