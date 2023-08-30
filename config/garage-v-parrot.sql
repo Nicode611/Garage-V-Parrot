b9d9e4a4e9ed3277d0be3eda3e5c4df383720ab2
Nicode611 <nicode611@gmail.com>
2023-08-30 17:34:45 +0200

    17:34

--- config/garage-v-parrot.sql
+++ config/garage-v-parrot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 29 août 2023 à 21:36
+-- Généré le : mer. 30 août 2023 à 17:33
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `garage-v-parrot`
+-- Base de données : `garage_v_parrot`
 --
 
 -- --------------------------------------------------------
@@ -28,13 +28,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `vehicules` (
-  `modele` int(11) NOT NULL,
-  `année` int(11) NOT NULL,
-  `kilométrage` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `modele` char(50) NOT NULL,
+  `année` int(4) NOT NULL,
+  `kilométrage` int(7) NOT NULL,
   `prix` int(11) NOT NULL,
-  `description` int(11) NOT NULL,
-  `image` int(11) NOT NULL
+  `description` text NOT NULL,
+  `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `vehicules`
+--
+
+INSERT INTO `vehicules` (`id`, `modele`, `année`, `kilométrage`, `prix`, `description`, `image`) VALUES
+(1, 'Skoda Fabia', 2015, 150000, 6700, 'Skoda Fabia de 2015 en tres bon état', 'https://zebi/zebi.com');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `vehicules`
+--
+ALTER TABLE `vehicules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `vehicules`
+--
+ALTER TABLE `vehicules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
