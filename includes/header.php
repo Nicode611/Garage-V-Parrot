20eaa95540fff45be7bdb159a02b0cdc4038cc82
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-07 17:37:58 +0200

    v

--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,7 @@
     <div class="nav-container">
         <nav>
             <ul class="nav-ul">
-                <a class="nav-element accueil" href="<?php echo ($_SERVER["DOCUMENT_ROOT"] . "/index.php"); ?>"><li>ACCUEIL</li></a>
+                <a class="nav-element accueil" href="index.php"><li>ACCUEIL</li></a>
                 <a class="nav-element services" href="pages/services.php"><li>SERVICES</li></a>
                 <a class="nav-element occasions" href="pages/occasions.php"><li>OCCASIONS</li></a>
                 <a class="nav-element contact" href="app/pages/contact.php"><li>CONTACT</li></a>
