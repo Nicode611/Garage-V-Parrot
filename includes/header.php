7e642dc95499de41fc5758c67f819b076cbe58d8
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-07 17:03:20 +0200

    17:03

--- includes/header.php
+++ includes/header.php
@@ -30,10 +30,10 @@
     <div class="nav-container">
         <nav>
             <ul class="nav-ul">
-                <a class="nav-element accueil" href="/Garage-V-Parrot/index.php"><li>ACCUEIL</li></a>
-                <a class="nav-element services" href= <?php ROOT . "pages/services.php" ?> ><li>SERVICES</li></a>
-                <a class="nav-element occasions" href="pages/occasions.php"><li>OCCASIONS</li></a>
-                <a class="nav-element contact" href="/Garage-V-Parrot/pages/contact.php"><li>CONTACT</li></a>
+                <a class="nav-element accueil" href=<?php ROOT . "/index.php" ?>><li>ACCUEIL</li></a>
+                <a class="nav-element services" href= <?php ROOT . "/pages/services.php" ?> ><li>SERVICES</li></a>
+                <a class="nav-element occasions" href=<?php ROOT . "/pages/occasions.php" ?>><li>OCCASIONS</li></a>
+                <a class="nav-element contact" href=<?php ROOT . "/pages/contact.php" ?>><li>CONTACT</li></a>
             </ul>
         </nav>
     </div>
