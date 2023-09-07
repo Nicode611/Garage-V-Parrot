88836deffb2f1f49351558edf0f746df90b52a00
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-07 17:21:40 +0200

    ,ve

--- includes/header.php
+++ includes/header.php
@@ -30,10 +30,10 @@
     <div class="nav-container">
         <nav>
             <ul class="nav-ul">
-                <a class="nav-element accueil" href=<?php (ROOT . "/index.php") ?>><li>ACCUEIL</li></a>
-                <a class="nav-element services" href= <?php (ROOT . "/pages/services.php"); ?> ><li>SERVICES</li></a>
-                <a class="nav-element occasions" href=<?php (ROOT . "/pages/occasions.php"); ?>><li>OCCASIONS</li></a>
-                <a class="nav-element contact" href="<?php ($_SERVER["DOCUMENT_ROOT"] . "/pages/contact.php"); ?>"><li>CONTACT</li></a>
+                <a class="nav-element accueil" href="<?php echo ($_SERVER["DOCUMENT_ROOT"] . "index.php"); ?>"><li>ACCUEIL</li></a>
+                <a class="nav-element services" href="<?php echo ($_SERVER["DOCUMENT_ROOT"] . "/pages/services.php"); ?>"><li>SERVICES</li></a>
+                <a class="nav-element occasions" href="<?php echo ($_SERVER["DOCUMENT_ROOT"] . "/pages/occasions.php"); ?>"><li>OCCASIONS</li></a>
+                <a class="nav-element contact" href="<?php echo ($_SERVER["DOCUMENT_ROOT"] . "/pages/contact.php"); ?>"><li>CONTACT</li></a>
                 <?php echo ($_SERVER["DOCUMENT_ROOT"] . "/pages/contact.php"); ?>
             </ul>
         </nav>
