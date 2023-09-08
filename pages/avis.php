e325c52ca1907e7e76a6b2e460641a8f940b813f
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-09 01:10:45 +0200

    01:10

--- pages/avis.php
+++ pages/avis.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/header.css">
     <link rel="stylesheet" href="../assets/css/global.css">
     <link rel="stylesheet" href="../assets/css/avis.css">
--- pages/dashboard-employes.php
+++ pages/dashboard-employes.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/dashboard-employes.css">
     <link rel="stylesheet" href="../assets/css/dashboard-admin.css">
     <link rel="stylesheet" href="../assets/css/global.css">
