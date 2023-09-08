3d44d0bbcbaa5fa22d0a219fd8babd12d5008a49
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-09 01:05:51 +0200

    01:05

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/global.css">
+    <link rel="icon" href="logo.ico" type="image/x-icon">
     <title>Garage V. Parrot</title>
 </head>
 <body>
@@ -20,7 +21,7 @@
     <!-- Présentation -->
     <div class="section-presentation">
         <div class="presentation-glow"></div>
-        <h2>Bienvenue au Garooage V. Parrot !</h2>
+        <h2>Bienvenue au Garage V. Parrot !</h2>
         <p class="presentation-text">
         Nous sommes votre destination de confiance pour tout ce qui concerne votre voiture.<br><br>
         Avec 15 ans d'expertise, nous offrons des services de réparation, d'entretien et de vente de véhicules d'occasion. Chez nous, votre voiture est entre de bonnes mains.<br><br>
