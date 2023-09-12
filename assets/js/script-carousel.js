7b32179bc6ab3ca527eacf50fb7bd15a33bd15d9
Nicode611 <nicode611@gmail.com>
2023-09-12 18:47:52 +0200

    18:47

--- assets/js/script-carousel.js
+++ assets/js/script-carousel.js
@@ -3,12 +3,13 @@ const carousel = document.querySelector('.avis-caroussel');
 const slides = document.querySelectorAll('.avis');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-const slideWidth = slides[0].offsetWidth; // Largeur d'une diapositive
-let currentIndex = Math.floor(slides.length / 2); // Commencez au milieu
+let currentIndex = Math.floor(slides.length / 2);
 
 // Fonction pour afficher la diapositive actuelle
 function showSlide(index) {
-    const translateX = -index * slideWidth; // Calcul du déplacement horizontal
+    const slideWidth = slides[0].offsetWidth + 100; // Largeur d'une diapositive
+    const middleSlideIndex = Math.floor(slides.length / 2); // Indice de la diapositive du milieu
+    const translateX = (middleSlideIndex - index) * slideWidth; // Calcul du déplacement horizontal
 
     slides.forEach((slide, i) => {
         slide.style.transform = `translateX(${translateX}px)`; // Applique la transformation
@@ -28,5 +29,3 @@ nextBtn.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % slides.length;
     showSlide(currentIndex);
 });
-
-
--- scripts/affichage/affichage-index-avis.php
+++ scripts/affichage/affichage-index-avis.php
@@ -9,21 +9,24 @@
         die("La connexion à la base de données a échoué : " . $conn->connect_error);
     }
 
-    $sql = "SELECT avis, nom FROM avis";
+    $sql = "SELECT avis, nom, statut FROM avis";
     $result = $conn->query($sql);
 
     // Si résultat plus grand que 0 lignes
     if ($result->num_rows > 0) {
         // Crée un var a chaque éléments
         while ($row = $result->fetch_assoc()) {
+            $statut = $row["statut"];
             $avis = $row["avis"];
-            $nom = $row["nom"];?>
+            $nom = $row["nom"];
 
-            <div class="avis">
-                <p class="avis-text"><?php echo $avis ?></p><br>
-                <p class="avis-name"><?php echo $nom ?></p>
-            </div>
-            <?php
+            if ($statut == "valid") {  ?>
+                <div class="avis">
+                    <p class="avis-text"><?php echo $avis ?></p><br>
+                    <p class="avis-name"><?php echo $nom ?></p>
+                </div>
+                <?php
+            }
         }
     } else {
         echo "Aucun avis trouvé.";
