feffaa8c852db8ea4659c65f5e7acba5abd8e32b
Nicode611 <nicode611@gmail.com>
2023-09-17 04:04:27 +0200

    04:04

--- pages/services.php
+++ pages/services.php
@@ -15,16 +15,18 @@
     $includeFile = "../includes/header.php";
     if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
 ?>
-
-<?php
-    $includeFile = "../includes/service.php";
-    if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
-?>
-
-<?php
-    $includeFile = "../includes/footer.php";
-    if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
-?>
-
+<div class="bla">
+    
+    <?php
+        $includeFile = "../includes/service.php";
+        if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
+    ?>
+    
+    <?php
+        $includeFile = "../includes/footer.php";
+        if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
+    ?>
+    
+</div>
 </body>
 </html>
---
+++ readme.md
@@ -0,0 +1,24 @@
+--Création d'un utilisateur :--
+
+Pour créer un nouvel utilisateur l'admin doit se connecter et aller dans son dashboard puis aller dans la section "employés".
+Il lui faut maintenant remplir les champs du formulaire et définir un code unique au nouvel utilisateur qu'il utilisera afin de se créer un compte avec l'adresse email et le mdp qu'il souhaite.
+
+Deux comptes admin et employés ont déja été créés afin de tester toutes les fonctionnalitées :
+
+identifiants Admin :
+-valentin.parrot@gmail.com
+-admin12!
+
+identifiants Admin de secours:
+-secours.admin@gmail.com
+-secours12!
+
+
+identifiants Employé :
+-employe@gmail.com
+-employe12!
+
+
+identifiants Employé de secours:
+secours.employe@gmail.com
+secoursemploye12!
