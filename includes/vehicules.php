2f22417a382f15510db7d9f284134f62cfb10ce7
SylverZ <127538225+Nicode611@users.noreply.github.com>
2023-09-18 03:21:25 +0200

    03:21

--- includes/vehicules.php
+++ includes/vehicules.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../assets/css/occasions.css">
         <title>Occasions</title>
-    </head>    
+    </head>
+    <?php 
+        $current_page = $_SERVER['REQUEST_URI'];
+        $page_name = basename($current_page);
+    ?>    
     <div class="section-vehicules-occasions">
         <div class="occasions-glow"></div>
             <div class="vehicules">
@@ -13,11 +17,29 @@
                     <h2>Véhicules d'occasions</h2>
                     <p>Nos véhicules d’occasion disponibles à l’achat.</p>
                 </div>
+                <?php
+                    if ($page_name == "occasions.php") { ?>
+                        <form id="form-filter">
+                            <div class="filter-options">
+                                <div>
+                                    <label for="price">Prix</label>
+                                    <input type="range" name="price" id="price">
+                                </div>
+                                <div>
+                                    <label for="year">Année</label>
+                                    <input type="number" name="year" id="year">
+                                </div>
+                                <div>
+                                    <label for="km">Kilometrage</label>
+                                    <input type="number" name="km" id="km">
+                                </div>
+                            </div>
+                            <button type="button" id="filter">Appliquer le filtre</button>
+                        </form>
+                    <?php } ?>
                 <div class="vehicules-imgs">
                     
                     <?php
-                        $current_page = $_SERVER['REQUEST_URI'];
-                        $page_name = basename($current_page);
                         if ($page_name == "index.php") {
                             $includeFile = ("scripts/affichage/show-vehicules.php");
                             if (file_exists($includeFile)) { include($includeFile); } else { echo "Le fichier $includeFile n'a pas été trouvé."; }
